4ed0261ed84583a5ead73fbeeef6cd834e1d9b57
enryu-maker <akifkhan60067@gmail.com>
2024-04-07 10:54:58 +0530

    cc

--- src/Assets/Images.js
+++ src/Assets/Images.js
@@ -1,5 +1,6 @@
 export const Images = {
     logo1 : require('./Image/logo1.png'),
-    logo2 : require('./Image/logo2.png')
+    logo2 : require('./Image/logo2.png'),
+    slogan : require('./Image/slogan.gif')
 
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,39 +1,20 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Images } from '../Assets/Images'
 export default function Footer() {
-    const [show, setShow] = React.useState(false)
     return (
         <div
-            className='bg-black w-full h-[100vh] flex flex-col justify-evenly '
+            className='bg-black w-full h-[100vh] flex flex-col justify-between '
         >
-            {
-                !show ?
                     <motion.div
-                        // whileInView={() => { setView(false) }}
-                        onHoverStart={() => {
-                            setShow(!show)
-                        }
-                        }
-                        onHoverEnd={() => {
-                            setShow(!show)
-                        }}
-                        // animate={{ opacity: show? 0 :  1 }}
                         transition={{ duration: 1, delay: .5 }}
-                        className='cursor-pointer transition font-MundoBold text-black bg-white w-full py-16 text-center text-6xl font-bold'>
-                        Let's grow together.
-                    </motion.div> :
-                    <motion.div
-                        onHoverStart={() => {
-                            setShow(!show)
-                            // setView(true)
-                        }}
-                        // onHoverEnd={() => setView(false)}
-                        // animate={{ opacity: show? 0 :  1 }}
-                        transition={{ duration: 1, delay: .5 }}
-                        className=' cursor-pointer transition font-MundoBold w-full text-white bg-black self-center py-16 text-center text-6xl font-bold'>
-                        We love your company<span className='font-light'>&#174;</span>.
+                        className=' cursor-pointer flex transition justify-center items-center font-MundoBold w-full py-5  text-white bg-black self-center text-center text-6xl font-bold'>
+                        <img 
+                        src={Images.slogan}
+                            className='object-contain'
+                        />
+                        &#174;.
                     </motion.div>
-            }
         </div>
     )
 }
