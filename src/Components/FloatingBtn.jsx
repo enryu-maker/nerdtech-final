7969ae86dc751de887638336a7895071b18af7c4
enryu-maker <akifkhan60067@gmail.com>
2024-04-13 22:54:20 +0530

    cc

--- src/Components/FloatingBtn.jsx
+++ src/Components/FloatingBtn.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FaPen } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 export default function FloatingBtn() {
+  const navigate = useNavigate()
   return (
     <motion.button
       whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.85 }}
+      onClick={()=>{
+        navigate('/contact-us')
+      }}
       className=' bg-blue-500 rounded-full w-20 h-20 flex items-center justify-center '>
       <FaPen className=' text-xl text-white ' />
     </motion.button>
